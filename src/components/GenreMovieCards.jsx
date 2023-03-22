4b8b692fe47bfcefa04730dce47aad34225d9104
half6 <christofferhalf6@gmail.com>
2023-03-22 11:01:23 +0100

    Added genre section and sorting

---
+++ src/components/GenreMovieCards.jsx
@@ -0,0 +1,29 @@
+import {Typography, Box, Paper} from '@mui/material';
+import { Link } from "react-router-dom";
+import StarIcon from '@mui/icons-material/Star';
+export default function GenreMoviesCards(props){
+    const {details} = props
+    return(
+        <>
+            <Paper sx={{ bgcolor:'#f5f5f5',color:'black', display:'flex',flexDirection:{xs:'column', md:'row'}, maxWidth:'620px',p:1}}>
+                <Box sx={{ width:'65px' }}>
+                    <img className='genreimg' src={`http://image.tmdb.org/t/p/w500/${details.poster_path}`} />
+                </Box>
+                <Box sx={{ width:'100%', p:1 }}>
+                    <Box sx={{ display:'flex' }}>
+                        <Link className='top-rated-title genreLink' to={`/details/${details.id}`}>{details.original_title ?? details.name}</Link>
+                        <Typography  variant='subtitle'>{`\u00A0`}({details.release_date.substring(0,4)})</Typography>                    
+                    </Box>
+
+
+                    <Box sx={{ display:'flex',alignItems:'center',gap:'5px' }}>
+                        <StarIcon sx={{ color:'#dba506' }}  />
+                        <Typography sx={{ fontWeight:'bold' }}>{details.vote_average === 0 ? 'No rating yet' : details.vote_average }</Typography>
+                    </Box>
+                    <Typography variant='body1' sx={{ fontSize:'12px',mt:0.5 }}>{details.overview ?? ''}</Typography>
+                    <Typography variant='body2' sx={{ mt:0.5,fontWeight:'bold' }}>Votes: {details.vote_count === 0 ? 'No votes yet' : details.vote_count }</Typography>
+                </Box>
+            </Paper>
+        </>
+    )
+}
--- src/components/MoviesByGenre.jsx
+++ src/components/MoviesByGenre.jsx
@@ -3,18 +3,30 @@ import {key} from './key.jsx'
 import {useState, useEffect} from 'react'
 import {Typography, Box,Paper,Container,Button} from '@mui/material';
 import GenreCard from "./GenreCard.jsx";
-import TopRated from "./TopRated.jsx";
+import GenreMovieCards from "./GenreMovieCards.jsx";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 export default function MoviesByGenre(){
 const {get, loading} = useFetch(`https://api.themoviedb.org/3/`)
 const [movieList, setMovieList] = useState([])
 const [genre,setGenre] = useState([])
+const [sortedMovies, setSortedMovies] = useState([])
 
 const [genreValue, setGenreValue] = useState('')
 const [page, setPage] = useState(1)
+const [title, setTitle] = useState('')
 
 function handleGenreValue(e){
     setGenreValue(prevVal => {
         prevVal = e.currentTarget.value
+        return prevVal
+    })
+}
+function handleSelect(e){
+    setGenreValue(prevVal => {
+        prevVal = e.target.value
         return prevVal
     })
 }
@@ -29,17 +41,88 @@ function handleGenreValue(e){
             setGenre(genreData.genres)
         })
         .catch(error => console.log(error))
-    },[genreValue])
-
+    },[genreValue,page])
 
     let genreList = genre.map(genres => <GenreCard handleClick={handleGenreValue} key={genres.id} details={genres}/> )
-    let genreMovies = movieList.map(movies => <TopRated key={movies.id} details={movies}/>)
+    let genreMovies = sortedMovies.map(movies => <GenreMovieCards key={movies.id} details={movies}/>)
+
+    function handlePageIncrement(){
+        setPage(prevVal=>{
+            prevVal ++
+            return prevVal
+        })
+    }
+    function handlePageDecrement(){
+        setPage(prevVal => {
+            if(prevVal > 1){
+                prevVal --
+            }
+            return prevVal
+        })
+    }
+
+    
+    useEffect(()=>{
+        let title 
+        if(genre !== []){
+            let genreCopy = [...genre]
+
+            genreCopy.filter(name =>{
+                
+                if(name.id === Number.parseInt(genreValue)){
+                    title = name.name
+                }
+                setTitle(title)
+                console.log(title)
+            })
+        }
+
+    },[genreValue])
+
+    useEffect(()=>{
+        if(movieList !== []){
+            let sorted = [...movieList]
+            sorted.sort((a,b)=>{
+              return  b.vote_average - a.vote_average
+            })
+            setSortedMovies(sorted)
+        }
+    },[movieList])
+
+    console.log(sortedMovies)
     return(
         <>
-        <Container sx={{ display:'flex', flexWrap:'wrap', gap:'10px',justifyContent:'center',alignItems:'center',mt:5 }}>
+        
+        
+        <Container maxWidth="md" sx={{p:1,background:'white',borderRadius:2, display:'flex', flexDirection:'column',mt:5,mb:5}}>
+            <Typography variant='h4' component='h1' sx={{ color:'black' }}>{title}</Typography>
+        {genreValue &&
+            <Box>
+                <FormControl sx={{ m: 1, minWidth: 80 }}>
+                    <InputLabel id="demo-simple-select-autowidth-label">Genre</InputLabel>
+                    <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value={genreValue}
+                    onChange={handleSelect}
+                    autoWidth
+                    label="Genre"
+                    >
+                    {genre.map(menuItem => <MenuItem key={menuItem.id} value={menuItem.id}>{menuItem.name}</MenuItem>)}
+                    </Select>
+                </FormControl>
+            </Box>}
+        <Box sx={{display:'flex', flexWrap:'wrap', gap:'10px',justifyContent:'center',alignItems:'center',mt:5  }}>
             {genreValue === '' && genreList }
             {genreValue && genreMovies}
-            {genreValue && <Button onClick={()=> setGenreValue('')}>Back</Button>}
+
+
+        </Box>
+        
+            {genreValue && <Box sx={{ display:'flex', justifyContent:'space-around',m:1 }}>
+                <Button disabled={page === 1} sx={{ color:'black',fontWeight:'bold' }} onClick={handlePageDecrement}>Previous</Button>
+                <Button sx={{ color:'black',fontWeight:'bold' }} onClick={handlePageIncrement}>Next</Button> 
+            </Box>}
         </Container>
         </>
     )
