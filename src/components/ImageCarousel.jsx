26a76ba27a2a3d9adecdfea57cfbbee0a352bdc6
half6 <christofferhalf6@gmail.com>
2023-05-04 11:25:12 +0200

    Update ImageCarousel.jsx

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -9,7 +9,7 @@ export default function ImageCarousel({details})
         <Paper sx={{bgcolor:'rgb(0, 0, 0)',mt:2, width:{xs:'100%' } }} >
           <Box sx={{display:'flex', flexDirection:'column', alignItems:'center' }}>
             <Box sx={{width:{xs:'100%', md:'100%'}, position:'relative' }}>
-                <img src={`http://image.tmdb.org/t/p/w500/${details.backdrop_path}`} className='caro-img' alt={details.title ?? details.name}/>
+                <img src={`https://image.tmdb.org/t/p/w500/${details.backdrop_path}`} className='caro-img' alt={details.title ?? details.name}/>
             </Box>
             <Box sx={{width:{xs:'60px', sm:'100px',md:'120px'}, position:'absolute', display:{xs:'none', md:'block'}, bottom:'40px',left:'10px' }}>
                 <img src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`} className='absolute-img caro-img' alt={details.title ?? details.name}/>
